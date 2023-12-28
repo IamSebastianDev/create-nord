f95caca1c2aa717f09d3c21d46a558b00a7944db
Sebastian Heinz <sebsatian@iamsebastian.dev>
2023-12-28 12:06:03 +0100

    feat: add a typescript vite nørd template

---
+++ template-vite-ts/src/components/greeting.component.ts
@@ -0,0 +1,20 @@
+/** @format */
+
+import { combined, createComponent, grain, on } from '@grainular/nord';
+import { greetings } from '../grains/greetings.grain';
+
+export const Greeting = createComponent((html) => {
+    const index = grain(0);
+
+    // Deriving the correct greeting from the currently set index
+    const greeting = combined([index, greetings], ([index, greetings]) => greetings[index % greetings.length]);
+
+    // Function to update the index by either adding or subtracting a value
+    const shiftIdx = (value: number) => index.update((i) => i + value);
+
+    return html`<div class="layout-row layout-medium">
+        <button ${on('click', () => shiftIdx(-1))}>&#171;</button>
+        <h1 title=${index}>${greeting}</h1>
+        <button ${on('click', () => shiftIdx(1))}>&#187;</button>
+    </div>`;
+});
---
+++ template-vite-ts/src/grains/greetings.grain.ts
@@ -0,0 +1,18 @@
+/** @format */
+
+import { grain } from '@grainular/nord';
+
+export const greetings = grain([
+    'Hej Nørd!',
+    'Hello Nørd!',
+    'Hallo Nørd!',
+    'Hei Nørd!',
+    'Terve Nørd!',
+    'Hallå Nørd!',
+    'Bonjour Nørd!',
+    'Ciao Nørd!',
+    'Hola Nørd!',
+    'Olá Nørd!',
+    'Dag Nørd!',
+    'Sveiki Nørd!',
+]);
